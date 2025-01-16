5389d9e854e6e528cfe668d2b41ae5eaf92543ec
ZanderKoch <zander.koch02@gmail.com>
2025-01-16 08:41:48 +0100

    Add Currency enum

---
+++ src/main/java/zander/lexicon/currencyconverter/Currency.java
@@ -0,0 +1,24 @@
+package zander.lexicon.currencyconverter;
+
+public enum Currency {
+    //taken from https://en.wikipedia.org/wiki/ISO_4217#Active_codes_(list_one)
+    USD ("United States dollars"),
+    SEK ("Swedish kronor"),
+    EUR ("Euro"),
+    DKK ("Danish kroner"),
+    NOK ("Norwegian kroner"),
+    ISK ("Icelandic krónur"),
+    GBP ("Pound sterling");
+
+
+    public final String name;
+
+    private Currency(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return this.name;
+    }
+}
