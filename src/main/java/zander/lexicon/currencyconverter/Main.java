8f4fa956eeea5ea23d1397ddb5fa2134a44db19c
ZanderKoch <zander.koch02@gmail.com>
2025-01-16 07:42:48 +0100

    Move user prompting and time printing to methods

--- src/main/java/zander/lexicon/currencyconverter/Main.java
+++ src/main/java/zander/lexicon/currencyconverter/Main.java
@@ -16,71 +16,21 @@ public class Main {
                 0. Exit
                 """);
 
-        boolean exiting = false;
-        while (!exiting) {
+        while (true) {
 
-            Scanner sc = new Scanner(System.in);
 
-            //ask user what to do
-            int chosenOption = 0;
-            boolean optionChosen = false;
-            while (!optionChosen) {
-                System.out.println("Enter your choise:");
-                String input = sc.nextLine();
-
-                int inputNum;
-                try { //check that it's a number
-                    inputNum = Integer.parseInt(input);
-                } catch (NumberFormatException e) {
-                    System.out.println("Please enter a numeric integer between 0 and 4 (both inclusive)!");
-                    continue;
-                }
-
-                if(inputNum == 0){
-                    exiting = true;
-                    break;
-                }
-
-                if (!(0 <= inputNum && inputNum <= 4)) {
-                    System.out.println("Please enter a numeric integer between 0 and 4 (both inclusive)!");
-                } else {
-                    chosenOption = inputNum;
-                    optionChosen = true;
-                }
-            }
-            //abort immediately if they wanted to exit
-            if (exiting){
+            int chosenOption = getOption();
+            if (chosenOption == 0) {
                 break;
             }
 
-            //ask user for amount of original currency
-            double chosenOriginalAmount = 0;
-            boolean originalAmountChosen = false;
-            while (!originalAmountChosen) {
-                System.out.println("Enter amount of original currency:");
-                String input = sc.nextLine();
-
-                double inputNum;
-                try { //check that it's a number
-                    inputNum = Double.parseDouble(input);
-                } catch (NumberFormatException e) {
-                    System.out.println("Please enter a positive numeric  number!");
-                    continue;
-                }
-
-                if (inputNum <= 0){
-                    System.out.println("Please enter a positive numeric  number!");
-                } else {
-                    chosenOriginalAmount = inputNum;
-                    originalAmountChosen = true;
-                }
-            }
+            double chosenOriginalAmount = getOriginalAmount();
 
             //set conversion rate and currency names based on previously chosen option
             String originalCurrencyName;
             String outputCurrencyName;
             double conversionRate;
-            switch (chosenOption){
+            switch (chosenOption) {
                 case 1:
                     originalCurrencyName = "SEK";
                     outputCurrencyName = "USD";
@@ -113,11 +63,75 @@ public class Main {
                     outputCurrencyName);
 
             //print current time for some reason
-            LocalDateTime current = LocalDateTime.now();
-            String formattedTime = current.format(DateTimeFormatter.ofPattern("MMMM dd, yyy, HH:mm:ss"));
-            System.out.println(formattedTime);
+            printCurrentTime();
         }
     }
 
+    private static void printCurrentTime() {
+        LocalDateTime current = LocalDateTime.now();
+        String formattedTime = current.format(DateTimeFormatter.ofPattern("MMMM dd, yyy, HH:mm:ss"));
+        System.out.println(formattedTime);
+    }
 
+    /**
+     * Prompts user to enter a number corresponding to an option using a system.in scanner
+     * until the user enters a valid one and returns the first valid choise.
+     *
+     * @return The first valid number the user enters
+     */
+    private static int getOption() {
+        Scanner sc = new Scanner(System.in);
+        int chosenOption = 0;
+        while (true) {
+            System.out.println("Enter your choise:");
+            String input = sc.nextLine();
+
+            int inputNum;
+            try { //check that it's a number
+                inputNum = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a numeric integer between 0 and 4 (both inclusive)!");
+                continue;
+            }
+
+            if (!(0 <= inputNum && inputNum <= 4)) {
+                System.out.println("Please enter a numeric integer between 0 and 4 (both inclusive)!");
+            } else {
+                chosenOption = inputNum;
+                break;
+            }
+        }
+        return chosenOption;
+    }
+
+    /**
+     * Prompts user to enter the amount of the original currency to be converted using a system.in scanner until the
+     * user enters a positive number and returns the first one the user enters
+     *
+     * @return The first positive number the user enters
+     */
+    private static double getOriginalAmount() {
+        Scanner sc = new Scanner(System.in);
+        double chosenOriginalAmount = 0;
+        while (true) {
+            System.out.println("Enter amount of original currency:");
+            String input = sc.nextLine();
+
+            double inputNum;
+            try { //check that it's a number
+                inputNum = Double.parseDouble(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a positive numeric number!");
+                continue;
+            }
+
+            if (inputNum <= 0) {
+                System.out.println("Please enter a positive numeric number!");
+            } else {
+                chosenOriginalAmount = inputNum;
+                break;
+            }
+        }
+        return chosenOriginalAmount;
+    }
 }
